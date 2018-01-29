5f435db5d6459fe6d63c0f4c0e196af2cd476026
eleve <eleve@si-PC100>
2018-01-29 17:04:48 +0100

    j ai fini exo 3

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -3,8 +3,12 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-
-
-
+int main()
+{
+	double r1;
+	r1 = exo1(10.0);
+	Affichage sur un terminal
+		std : : cout << "Hello World!" << std : : end1;
+	return 0;
+}
 #endif // MY_HEADER_HPP
-
