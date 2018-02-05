c19fa34928a4b50d3fd67a739939c47fa0d9b234
eleve <eleve@si-PC100>
2018-02-05 17:11:52 +0100

    nous avons fini exo 1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,9 +1,18 @@
-//  Copyright (c) 2017 Antoine Tran Tan
+//  Copyright (c) 2017  Ayoub CHAHHIR
 //
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-
+void exo1(void)
+{
+	long a = 2, b = 1, c = 0;
+	while (a + b > 0)
+	{
+		a = a - 2;
+		b = b + 1;
+		c = c + 1;
+	}
+}
 
 
 #endif // MY_HEADER_HPP
--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,14 @@
-//  Copyright (c) 2018 Antoine Tran Tan
+//  Copyright (c) 2018 Ayoub CHAHIR
 //
 
 #include <iostream>
 #include <my_header.hpp>
-
 int main()
 {
-    // Affichage sur un terminal
-    std::cout << "Hello World!" << std::endl;
+	exo1();
 
-    return 0;
+	return 0;
 }
+
+
+
