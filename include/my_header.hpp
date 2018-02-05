bf902ca02fd4482477cc07033a13c07c1f7c1dee
ayoubchahir <chahir.ayoub@hotmail.fr>
2018-02-05 18:27:27 +0100

    on a fini exo 3 #Ayoub le bg

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -3,6 +3,10 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
+
+#include <stdlib.h>
+#include <time.h>
+
 void exo1(void)
 {
 	long a = 2, b = 1, c = 0;
@@ -14,6 +18,33 @@ void exo1(void)
 	}
 }
 
+int essayer_code(unsigned short code)
+{
+	srand(time(NULL));
+	static unsigned short guess = rand() % 10000;
+
+	return (code == guess);
+}
+
+unsigned short lancer_de()
+{
+	srand(time(NULL));
+	return (rand() % 6) + 1;
+}
+
+void exo3(void)
+{
+
+}
+
+void exo4.(void)
+{
+
+}
 
 #endif // MY_HEADER_HPP
+
+
+
+
 
